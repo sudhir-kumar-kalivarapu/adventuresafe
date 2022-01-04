c4620e898f4926399ddb3d7b9fe1806596a68797
sudhir-kumar-kalivarapu <91311037+sudhir-kumar-kalivarapu@users.noreply.github.com>
2022-01-04 11:29:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,29 +3,16 @@ import Card from './Card';
 import Navbar from './Header';
 import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
 import ProtectedRoutes from './ProtectedRoutes';
-
-const useAuth =()=>{
-  const user={ signedIn: localStorage.getItem('Name') ? true : false };
-  return user && user.signedIn;
-};
-
-const authCheck =() => {
-  console.log('hgvhgvhgv   ', useAuth);
-  if (localStorage.getItem('Name')) {
-    return (
-      <Navbar />
-    );
-  } else {
-    <Card />
-  }
-};
+import ProtectedRoutes1 from './ProtectedRoutes1';
 
 const App=()=> {
   return (
     <div className="App" style={{backgroundImage:"url(/images/background.jpg)"}}>
        <Router>
         <Routes>
-        <Route path='/' element={ useAuth() ? < Navbar /> : <Card />}></Route>
+        <Route element={<ProtectedRoutes1/>}>
+        <Route path='/' element={<Card />}></Route>
+        </Route>
         <Route element={<ProtectedRoutes/>}>
           <Route path='/dashboard' element={ <Navbar />}></Route>
         </Route>
@@ -35,4 +22,3 @@ const App=()=> {
   );
 }
 export default App;
-
